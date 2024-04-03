423b962f4e8f860e1ab3fc5298b60973900fd580
Jbtzcodes <laminutedecode@gmail.com>
2024-04-03 16:27:54 +0200

    modifications

--- 16-Youtube-clone/src/utils/constants.ts
+++
@@ -1,1 +0,0 @@
-export const YOUTUBE_API_URL= "https://youtube.googleapis.com/youtube/v3"
--- 16-Youtube-clone/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { convertRawViewstoString } from "./convertRawViewsToString";
-export { parseVideoDuration } from "./parseVideoDuration";
-export { timeSince } from "./timeSince";
-export { parseData } from "./parseData";
-export { parseRecommendedData } from "./parseRecommendedData";
